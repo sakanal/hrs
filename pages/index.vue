9aec6f21238f3999cdbe4e3fccb1e8313fe099cf
sakanal <sakanal9527@gmail.com>
2023-02-10 18:43:58 +0800

    项目初始化

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NuxtLogo/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  async asyncData({$axios}) {
+    let [data] = await Promise.all([
+      $axios.get(`/admin/house/housearea/list`),
+    ])
+    return {data}
+  },
+  created() {
+    this.$axios.get(`/admin/house/housearea/list`).then(response => {
+      console.log(response)
+    })
+  }
+}
+</script>
