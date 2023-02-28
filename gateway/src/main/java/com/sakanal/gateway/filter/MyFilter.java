eba7c2004591014e628d0df8516027805d29b762
sakanal <sakanal9527@gmail.com>
2023-02-28 23:09:05 +0800

    建立Gateway统一鉴权模型

---
+++ gateway/src/main/java/com/sakanal/gateway/filter/MyFilter.java
@@ -0,0 +1,30 @@
+package com.sakanal.gateway.filter;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.RequestPath;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class MyFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        RequestPath path = request.getPath();
+        String token = exchange.getRequest().getHeaders().getFirst("token");
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
