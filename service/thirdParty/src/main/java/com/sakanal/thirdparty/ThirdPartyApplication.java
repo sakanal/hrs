ed326ecf3214351f8a52ec4c79efa26423acf733
sakanal <sakanal9527@gmail.com>
2023-03-03 16:11:44 +0800

    添加第三方服务的基础框架

---
+++ service/thirdParty/src/main/java/com/sakanal/thirdparty/ThirdPartyApplication.java
@@ -0,0 +1,18 @@
+package com.sakanal.thirdparty;
+
+import org.redisson.spring.starter.RedissonAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, RedissonAutoConfiguration.class, RedisAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.sakanal.base", "com.sakanal.thirdparty"})
+public class ThirdPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdPartyApplication.class, args);
+    }
+
+}
