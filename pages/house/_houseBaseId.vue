2cea921d3a2ca518db5e967320a0651ad2ee38ee
sakanal <sakanal9527@gmail.com>
2023-03-26 11:31:01 +0800

    实现浏览功能

--- pages/house/_houseBaseId.vue
+++ pages/house/_houseBaseId.vue
@@ -297,6 +297,7 @@
 // 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 // 例如：import 《组件名称》 from ‘《组件路径》‘;
 import '../../assets/icon/iconfont.css'
+import cookie from 'js-cookie'
 
 export default {
   // import引入的组件需要注入到对象中才能使用
@@ -352,6 +353,9 @@ export default {
   watch: {},
   // 生命周期 - 创建完成（可以访问当前this实例）
   created () {
+    if (cookie.get('userToken')) {
+      this.$axios.post(`/user/userbrowse/browse/${this.houseBaseId}`)
+    }
   },
   // 生命周期 - 挂载完成（可以访问DOM元素）
   mounted () {
