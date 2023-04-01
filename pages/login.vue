36f089677ffd920105053d675ccae06f87c77d33
sakanal <sakanal9527@gmail.com>
2023-04-01 17:40:48 +0800

    完成手机号免密登录

--- pages/login.vue
+++ pages/login.vue
@@ -46,14 +46,35 @@
               <el-tab-pane label="手机验证码登录" name="phone">
                 <el-form ref="phoneLoginForm" :model="phoneLogin" :rules="phoneRules" status-icon label-width="0px"
                          class="demo-ruleForm">
-                  <el-form-item prop="userName">
-                    <el-input v-model="phoneLogin.userName" placeholder="手机号" autocomplete="off"/>
+<!--                  <el-form-item prop="userName">-->
+<!--                    <el-input v-model="phoneLogin.phone" placeholder="手机号" autocomplete="off"/>-->
+<!--                  </el-form-item>-->
+
+                  <el-form-item prop="phone">
+                    <el-input v-model="phoneLogin.phone" placeholder="手机号" auto-complete="false">
+                      <template slot="append">
+                        <template v-if="!hasCode">
+                          <el-button @click="getCode">获取动态码</el-button>
+                        </template>
+                        <template v-else>
+                          <span>重新获取({{time}})</span>
+                        </template>
+                      </template>
+                    </el-input>
                   </el-form-item>
                   <el-form-item prop="password">
-                    <el-input type="password" v-model="phoneLogin.password" placeholder="验证码" autocomplete="off"/>
+                    <el-input type="password" v-model="phoneLogin.code" placeholder="验证码" autocomplete="off"/>
+                  </el-form-item>
+                  <el-form-item>
+                    <el-checkbox-group v-model="simpleLogin.autoLogin">
+                      <el-checkbox label="autoLogin">7天免登录</el-checkbox>
+                    </el-checkbox-group>
+                    <div style="position: absolute;top: 0;right: 5px">
+                      <el-link :underline="false">忘记密码</el-link>
+                    </div>
                   </el-form-item>
                   <el-form-item size="medium">
-                    <el-button type="primary" style="min-width: 100%">登录</el-button>
+                    <el-button type="primary" style="min-width: 100%" @click="toPhoneLogin">登录</el-button>
                   </el-form-item>
                 </el-form>
               </el-tab-pane>
@@ -73,6 +94,26 @@ import cookie from 'js-cookie'
 export default {
   layout: 'login-register',
   data () {
+    const validatePhone = (rule, value, callback) => {
+      const pattern = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/
+      if (value === '') {
+        callback(new Error('请输入手机号'))
+      } else if (!pattern.test(value)) {
+        callback(new Error('请输入有效手机号'))
+      } else {
+        callback()
+      }
+    }
+    const validateCode = (rule, value, callback) => {
+      const pattern = /^\d{6}$/
+      if (value === '') {
+        callback(new Error('请输入动态码'))
+      } else if (!pattern.test(value)) {
+        callback(new Error('请输入有效动态码'))
+      } else {
+        callback()
+      }
+    }
     return {
       imgSrc: require('../static/image/login.jpg'),
       activeName: 'simple',
@@ -104,29 +145,33 @@ export default {
           }
         ]
       },
+      hasCode: false,
+      time: 60,
       phoneLogin: {
-        userName: '',
-        password: ''
+        phone: '',
+        code: '',
+        autoLogin: false
       },
       phoneRules: {
-        userName: [
+        phone: [
           {
             required: true,
-            message: '请输入用户名',
+            message: '请输入手机号',
             trigger: 'blur'
           },
-          // { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+          {
+            validator: validatePhone,
+            trigger: 'blur'
+          }
         ],
-        password: [
+        code: [
           {
             required: true,
-            message: '请输入密码',
+            message: '请输入动态码',
             trigger: 'blur'
           },
           {
-            min: 6,
-            max: 16,
-            message: '长度在 6 到 16 个字符',
+            validator: validateCode,
             trigger: 'blur'
           }
         ]
@@ -134,6 +179,32 @@ export default {
     }
   },
   methods: {
+    getCode () {
+      this.$refs.phoneLoginForm.validateField('phone', (valid) => {
+        if (!valid && !this.hasCode) {
+          this.$axios.get(`/user/login/sendCode/${this.phoneLogin.phone}`).then(response => {
+            if (response && response.code === 0) {
+              this.$message.success('动态码发送成功')
+              this.time=60
+              this.timer()
+              this.hasCode = true
+            } else {
+              this.$message.error(response.msg)
+            }
+          })
+        }
+      })
+    },
+    //发送手机验证码倒计时
+    timer() {
+      if (this.time > 1) {
+        this.time--;
+        setTimeout(this.timer, 1000);
+      } else{
+        this.time=0;
+        this.hasCode=false
+      }
+    },
     toSimpleLogin () {
       this.$refs.simpleLoginForm.validate((valid) => {
         if (valid) {
@@ -156,6 +227,33 @@ export default {
                 this.$router.push({ path: '/' })
               }
             })
+        } else {
+          this.$message.error('请正确输入登录信息！')
+          return false
+        }
+      })
+    },
+    toPhoneLogin(){
+      this.$refs.phoneLoginForm.validate((valid)=>{
+        if (valid){
+          let data={
+            'phone': this.phoneLogin.phone,
+            'code': this.phoneLogin.code,
+          }
+          this.$axios.post('/user/login/phone',data).then(response=>{
+            if (response!==undefined){
+              let expires=this.phoneLogin.autoLogin?7:1
+              cookie.set(
+                'userToken',
+                response.token,
+                {
+                  domain: 'localhost',
+                  expires: expires
+                }
+              )
+              this.$router.push({ path: '/' })
+            }
+          })
         } else {
           this.$message.error('请正确输入登录信息！')
           return false
