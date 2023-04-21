5711c42aad884e79c7288876d6f6f39888e3c60a
sakanal <sakanal9527@gmail.com>
2023-04-21 23:59:56 +0800

    添加订单状态信息

---
+++ common/common_base/src/main/java/com/sakanal/base/constant/OrderStateConstant.java
@@ -0,0 +1,10 @@
+package com.sakanal.base.constant;
+
+public class OrderStateConstant {
+    //未支付-0
+    public static final Integer DEFAULT =  0;
+    //已支付-1
+    public static final Integer SUCCESS =  1;
+    //取消订单-2
+    public static final Integer FAIL =  2;
+}
