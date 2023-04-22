cbbf04d4c5a9e62cd64f5d116c65bef9a4fedacd
sakanal <sakanal9527@gmail.com>
2023-04-22 09:09:49 +0800

    整合支付宝支付功能

---
+++ common/common_service/src/main/java/com/sakanal/service/dto/PayDTO.java
@@ -0,0 +1,16 @@
+package com.sakanal.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PayDTO {
+    private String out_trade_no; // 商户订单号 必填
+    private String subject; // 订单名称 必填
+    private String total_amount;  // 付款金额 必填
+    private String body; // 商品描述 可空
+}
+
